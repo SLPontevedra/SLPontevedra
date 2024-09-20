e49e4c470578220cbfe4e437ddb3235556a2f45d
CodeSJMartinez <serviciosjmartinez@gmail.com>
2024-09-20 18:05:16 +0200

    Nuevo menú tipo hamburguesa para pantallas pequeñas.

--- assets/js/header.js
+++ assets/js/header.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
             <header>
                 <h1>SL Pontevedra Activa</h1>
                 <nav>
-                    <ul>
+                    <div class="menu-icon" id="menu-icon">&#9776;</div> <!-- Ícono hamburguesa -->
+                    <ul id="menu-list">
                         <li><a href="/index.html">Inicio</a></li>
                         <li><a href="/blog.html">Blog</a></li>
                         <li><a href="/site-map.html">Guía Rápida</a></li>
@@ -18,6 +19,14 @@ document.addEventListener("DOMContentLoaded", function() {
         // Inserta el header en el body
         const bodyElement = document.body;
         bodyElement.insertAdjacentHTML('afterbegin', headerTemplate);
+
+        // Lógica para el menú hamburguesa
+        const menuIcon = document.getElementById("menu-icon");
+        const menuList = document.getElementById("menu-list");
+
+        // Alternar el menú al hacer clic en el ícono hamburguesa
+        menuIcon.addEventListener("click", function() {
+            menuList.classList.toggle("show");
+        });
     }
 });
-
